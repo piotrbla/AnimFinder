bef3b6cb4d53a29205a0881e4c4e118f1fc119e2
PiotrB <piotrb@gmail.com>
2018-12-28 22:07:24 +0100

    Back from game window

--- Assets/QuitApp.cs
+++ Assets/QuitApp.cs
@@ -1,9 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class QuitApp : MonoBehaviour {
 	public void DoQuit () {
 		Application.Quit();
 	}
+
+    public void DoBack()
+    {
+        SceneManager.LoadScene("Main");
+    }
 }
